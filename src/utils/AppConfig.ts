41ca4a1d9500d8e1a823f80bbf1edeb7e7d292d5
DE ZORDO Benjamin <bdezordo@gmail.com>
2023-04-20 22:52:17 +0200

    ok

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Benjamin DE ZORDO',
+  title: 'Benjamin DE ZORDO',
+  description: 'My personal website',
+  author: 'Benjamin DE ZORDO',
   locale_region: 'en-us',
   locale: 'en',
 };
